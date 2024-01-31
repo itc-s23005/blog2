d658d83e9dcd57864aad7610500d2988e898a285
itc-s23005 <s23005@std.it-college.ac.jp>
2024-01-31 07:55:15 +0000

    2nd Chapter7-4 非同期処理

---
+++ pages/blog/schedule.js
@@ -0,0 +1,15 @@
+import { client } from 'lib/api'
+const Schedule = () => {
+  return <h1>記事のタイトル</h1>
+}
+
+const getStaticProps = async => {
+  const resPromise = client.get({ endpoint: 'blogs' })
+  console.log(resPromise)
+
+  return {
+    props: {}
+  }
+}
+export { getStaticProps }
+export default Schedule
